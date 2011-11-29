590893727d7bd676f373a5a196721ee42d56d7af
Thiago Nogueira <thiago.nogueira@actminds.com>
2011-11-29 08:28:22 -0300

    Create the user interface - Login window

---
+++ smileplug-adm-app/src/main/java/com/razortooth/smile/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.razortooth.smile.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.razortooth.smile.R;
+
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.login);
+    }
+}
