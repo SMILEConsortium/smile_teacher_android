fe05856468821f7ce296253453ab523b16d96ada
Marco Antonio Rocha <marco.rocha@actminds.com>
2011-11-30 18:24:36 -0200

    Implementing utility methods to load byte[] and adding tests to Base64

--- smileplug-adm-app/src/main/java/com/razortooth/smile/util/IOUtil.java
+++ smileplug-adm-app/src/main/java/com/razortooth/smile/util/IOUtil.java
@@ -1,23 +1,18 @@
 package com.razortooth.smile.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class IOUtil {
 
     private IOUtil() {
         // Empty
     }
-
-    // public static final void silentClose(InputStream is) {
-    // try {
-    // if (is != null) {
-    // is.close();
-    // }
-    // } catch (IOException e) {
-    // // Empty
-    // }
-    // }
 
     public static final void silentClose(Closeable c) {
         try {
@@ -27,6 +22,49 @@ public class IOUtil {
         } catch (IOException e) {
             // Empty
         }
+    }
+
+    public static byte[] loadBytesFromResource(String resource) throws IOException {
+        InputStream is = IOUtil.class.getResourceAsStream(resource);
+        return loadBytes(is);
+    }
+
+    public static byte[] loadBytes(InputStream is) throws IOException {
+
+        int len;
+        int size = 1024;
+        byte[] buf;
+
+        if (is instanceof ByteArrayInputStream) {
+            size = is.available();
+            buf = new byte[size];
+            len = is.read(buf, 0, size);
+        } else {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            buf = new byte[size];
+            while ((len = is.read(buf, 0, size)) != -1) {
+                bos.write(buf, 0, len);
+            }
+            buf = bos.toByteArray();
+        }
+        return buf;
+
+    }
+
+    public static byte[] loadBytes(File file) throws IOException {
+
+        long length = file.length();
+        if (length > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("File '" + file.getAbsolutePath() + "' is too large");
+        }
+
+        FileInputStream fis = new FileInputStream(file);
+        try {
+            return loadBytes(fis);
+        } finally {
+            silentClose(fis);
+        }
+
     }
 
 }
