f87fd0283170ea196e1e04070bd6ea41e2e7d8c1
Marco Antonio Rocha <marco.rocha@actminds.com>
2011-11-30 15:44:58 -0200

    Adding importQuestions method

--- smileplug-adm-app/src/main/java/com/razortooth/smile/bu/QuestionsManager.java
+++ smileplug-adm-app/src/main/java/com/razortooth/smile/bu/QuestionsManager.java
@@ -1,8 +1,13 @@
 package com.razortooth.smile.bu;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import android.util.Log;
@@ -61,6 +66,141 @@ public class QuestionsManager {
             // TODO: Exception?
         } finally {
             IOUtil.silentClose(pw);
+        }
+
+    }
+
+    protected Collection<Question> readQuestionsFromFile(String filePath) {
+
+        Collection<Question> result = new ArrayList<Question>();
+        Log.d("QuestionsManager", "Importing Questions: " + filePath);
+
+        File file = new File(filePath);
+
+        if (!file.exists()) {
+            Log.d("QuestionsManager", "File doesn't exists: " + filePath);
+            return result;
+        }
+
+        do {
+
+            FileReader fr = null;
+            try {
+                fr = new FileReader(file);
+            } catch (FileNotFoundException e1) {
+                // TODO: Exception?
+                return result;
+            }
+
+            BufferedReader br = new BufferedReader(fr);
+
+            try {
+
+                // Read Marker
+                if (!readMarker(br)) {
+                    break;
+                }
+
+                // Number of Questions
+                int n = Integer.valueOf(br.readLine());
+                if (!readMarker(br)) {
+                    break;
+                }
+
+                if (n <= 0) {
+                    return result;
+                }
+
+                Log.d("QuestionsManager", "Number of Questions: " + n);
+
+                for (int i = 0; i < n; i++) {
+
+                    int number;
+                    String question = "";
+                    String option1 = "";
+                    String option2 = "";
+                    String option3 = "";
+                    String option4 = "";
+                    int answer;
+                    String owner = "";
+                    boolean hasImage = false;
+
+                    String sNumber = readUntilMarker(br);
+                    number = Integer.valueOf(sNumber);
+                    if (number != i) {
+                        break;
+                    }
+
+                    question = readUntilMarker(br);
+                    option1 = readUntilMarker(br);
+                    option2 = readUntilMarker(br);
+                    option3 = readUntilMarker(br);
+                    option4 = readUntilMarker(br);
+
+                    String sAnswer = readUntilMarker(br);
+                    answer = Integer.valueOf(sAnswer);
+
+                    owner = readUntilMarker(br);
+
+                    String sHasImage = readUntilMarker(br);
+                    hasImage = sHasImage.equals("Y");
+
+                    String image = null;
+                    if (hasImage) {
+                        // TODO: Load Image
+                    }
+
+                    Question q = new Question();
+                    q.setNumber(number);
+                    q.setQuestion(question);
+                    q.setOption1(option1);
+                    q.setOption2(option2);
+                    q.setOption3(option3);
+                    q.setOption4(option4);
+                    q.setAnswer(answer);
+                    q.setOwner(owner);
+                    q.setImage(image);
+
+                    result.add(q);
+                }
+
+            } catch (IOException e) {
+                // TODO: Exception
+            } finally {
+                IOUtil.silentClose(br);
+            }
+
+        } while (false);
+
+        return result;
+    }
+
+    public boolean readMarker(BufferedReader br) throws IOException {
+        String s = br.readLine();
+        return s != null && s.equals(MARKER);
+    }
+
+    public String readUntilMarker(BufferedReader in) throws IOException {
+
+        String s = "";
+        boolean first = true;
+        while (true) {
+
+            String t = in.readLine();
+            if (t == null) {
+                return s;
+            }
+
+            if (t.equals(MARKER)) {
+                return s;
+            }
+
+            if (first) {
+                s += t;
+            } else {
+                s += "\n" + t;
+            }
+
         }
 
     }
