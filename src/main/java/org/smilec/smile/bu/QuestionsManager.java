a579153c17baa22e359008fe74b1eb5888890946
David J. Kordsmeier <dkords@gmail.com>
2013-05-22 15:39:34 -0700

    Add CSV support.  Mirrors approach used by Java Desktop App and uses existing approach to load images.

--- src/main/java/org/smilec/smile/bu/QuestionsManager.java
+++ src/main/java/org/smilec/smile/bu/QuestionsManager.java
@@ -19,8 +19,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -37,6 +40,9 @@ import org.smile.smilec.util.IOUtil;
 import org.smile.smilec.util.IPAddressUtil;
 import org.smile.smilec.util.ImageLoader;
 
+import com.csvreader.CsvReader;
+import com.csvreader.CsvWriter;
+
 public class QuestionsManager {
 
 	private static final String JPG = ".jpg";
@@ -45,6 +51,8 @@ public class QuestionsManager {
 	private static final String MARKER = "_@JSQ%_";
 	public static final String TEACHER_NAME = "teacher";
 	public static final String TEACHER_IP = "127.0.0.1";
+    public static final String IQ_MIME_EXT_JSON = ".json";
+    public static final String IQ_MIME_EXT_CSV = ".csv";
 
 	private final File questionsDir;
 
@@ -155,7 +163,7 @@ public class QuestionsManager {
 	}
 
 	public Collection<Question> loadQuestions(String name) {
-
+        int number = 1;
 		Collection<Question> result = new ArrayList<Question>();
 		Log.d("QuestionsManager", "Importing Questions: " + name);
 
@@ -163,17 +171,79 @@ public class QuestionsManager {
 		File file = new File(dir, QUESTIONS_FILE);
 
 		if (!file.exists()) {
-			Log.d("QuestionsManager", "Questions file doesn't exists: " + name);
-			return result;
+			Log.d("QuestionsManager", "JSON Questions file doesn't exist: " + file.getAbsolutePath());
+            file = new File(dir, dir.getName());
+		    if (!file.exists()) {
+			    Log.d("QuestionsManager", "CSV Questions file doesn't exist: " + file.getAbsolutePath());
+			    return result;
+            } else {
+			    Log.d("QuestionsManager", "CSV Questions file exists: " + file.getAbsolutePath());
+            }
 		}
 
-		do {
+        // XXX candidate for refactoring
+        if (file.getAbsolutePath().endsWith(IQ_MIME_EXT_CSV) == true) {
+		  Log.d("QuestionsManager", "Load CSV Questions from: " + file.getAbsolutePath());
+          //
+          // Handle the .csv load
+          //
+          try {
+            CsvReader csvQuestions = new CsvReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
+            csvQuestions.readHeaders();
+
+            while (csvQuestions.readRecord())
+            {
+              try {
+                String num = csvQuestions.get("num");
+                String question = csvQuestions.get("question");
+                String option1 = csvQuestions.get("choice1");
+                String option2 = csvQuestions.get("choice2");
+                String option3 = csvQuestions.get("choice3");
+                String option4 = csvQuestions.get("choice4");
+                String hasImage = csvQuestions.get("has_image");
+				String image = null;
+				if (hasImage != null && hasImage.length() > 0) {
+					File img = new File(dir, number + JPG);
+					byte[] originalArray = IOUtil.loadBytes(img);
+					String encodedString = Base64.encodeBytes(originalArray);
+					image = encodedString;
+				}
+                int answer = Integer.valueOf(csvQuestions.get("answers"));
+                String owner = csvQuestions.get("owner_name");
+                if (owner == null || owner.length() == 0) {
+                    owner= TEACHER_NAME;
+                }
+                String ip = csvQuestions.get("owner_IP");
+                if (ip == null || ip.length() == 0) {
+                    ip = TEACHER_IP;
+                }
+				Question q = new Question(number, owner, ip, question, option1, option2, option3, option4, answer, image);
+				result.add(q);
+                number = number + 1;
+              } catch(IOException ioe) {
+                  // XXX Log the error or do something
+				  Log.e(Constants.LOG_CATEGORY, "CSV Load Error: ", ioe);
+              }
+            }
+          } catch(FileNotFoundException fnfe) {
+                  // XXX Log the error or do something
+				  Log.e(Constants.LOG_CATEGORY, "CSV Load Error, cannot find file: ", fnfe);
+          } catch(UnsupportedEncodingException uee) {
+                  // XXX Log the error or do something
+				  Log.e(Constants.LOG_CATEGORY, "CSV Load Error, unsupported encoding: ", uee);
+          } catch(IOException ioe) {
+                  // XXX Log the error or do something
+				  Log.e(Constants.LOG_CATEGORY, "CSV Load Error: ", ioe);
+          }
+        } else {
+		  Log.d("QuestionsManager", "Load JSON Questions from: " + file.getAbsolutePath());
+		  do {
 
 			FileReader fr = null;
 			try {
 				fr = new FileReader(file);
 			} catch (FileNotFoundException e1) {
-				Log.e(Constants.LOG_CATEGORY, "Error: ", e1);
+				Log.e(Constants.LOG_CATEGORY, "File Load Error, not found: ", e1);
 				return result;
 			}
 
@@ -196,12 +266,11 @@ public class QuestionsManager {
 					return result;
 				}
 
-				Log.d("QuestionsManager", "Number of Questions: " + n);
 
 				for (int i = 0; i < n; i++) {
 
 					String sNumber = readUntilMarker(br);
-					int number = Integer.valueOf(sNumber);
+					number = Integer.valueOf(sNumber);
 
 					String question = readUntilMarker(br);
 					String option1 = readUntilMarker(br);
@@ -237,18 +306,20 @@ public class QuestionsManager {
 					}
 
 					Question q = new Question(number, owner, ip, question, option1, option2,
-						option3, option4, answer, image);
+					option3, option4, answer, image);
 
 					result.add(q);
 				}
 
 			} catch (IOException e) {
-				Log.e(Constants.LOG_CATEGORY, "Error: ", e);
+				Log.e(Constants.LOG_CATEGORY, "JSON File Load Error: ", e);
 			} finally {
 				IOUtil.silentClose(br);
 			}
 
-		} while (false);
+		  } while (false);
+		  Log.d("QuestionsManager", "Number of Questions: " + n);
+        }
 
 		return result;
 	}
