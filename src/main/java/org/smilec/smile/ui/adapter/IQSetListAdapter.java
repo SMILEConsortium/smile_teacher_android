23f9c7ad899fec2aec77f7bcdccc42b2661632e9
chrqls <charles.quelos@gmail.com>
2013-12-24 22:56:16 -0800

    [#4] Just adding a simple feature to see the number of preloaded questions per IQSet.

--- src/main/java/org/smilec/smile/ui/adapter/IQSetListAdapter.java
+++ src/main/java/org/smilec/smile/ui/adapter/IQSetListAdapter.java
@@ -3,8 +3,10 @@ package org.smilec.smile.ui.adapter;
 import java.util.List;
 
 import org.smilec.smile.R;
+import org.smilec.smile.bu.SmilePlugServerManager;
 import org.smilec.smile.domain.IQSet;
 
+import android.accounts.NetworkErrorException;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,10 +21,13 @@ import android.widget.TextView;
  * */
 public class IQSetListAdapter extends ArrayAdapter<IQSet> {
 	
+	private String ip;
+	
 	// We load the list of iqsets to retrieve the values in getView(,,)  
-	public IQSetListAdapter(Context context,List<IQSet> iqsets) {
+	public IQSetListAdapter(Context context,List<IQSet> iqsets, String ip) {
 		
 		super(context, 0, iqsets);
+		this.ip = ip;
 	}
 
 	// This method will be executed for each row
@@ -49,6 +54,18 @@ public class IQSetListAdapter extends ArrayAdapter<IQSet> {
             
             TextView tvDate = (TextView) convertView.findViewById(R.id.tv_date);
             tvDate.setText(String.valueOf(iqset.getKey()));
+            
+            String idIQSet;
+            int size = -1;
+			try {
+				
+				idIQSet = new SmilePlugServerManager().getIdIQSetByPosition(ip, getContext(), position);
+				size = new SmilePlugServerManager().getListOfQuestions(ip, getContext(), idIQSet).size();
+			} 
+			catch (NetworkErrorException e) { e.printStackTrace(); }
+            
+            TextView tvSize = (TextView) convertView.findViewById(R.id.tv_size);
+            tvSize.setText(String.valueOf(size));
         }
 
         return convertView;
